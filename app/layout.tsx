fec45cd092660a985a66cf14ce9950ac44f80658
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air.local>
2026-02-21 08:05:51 +0530

    fixed vercel syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script id="env-var-override" strategy="beforeInteractive">
+          {`
+          window.process = window.process || { };
+          window.process.env = window.process.env || { };
+          window.process.env.NEXT_PUBLIC_FIREBASE_API_KEY = "AIzaSyA4MGOnuikepIa2KTKWsjXFdibYRa8npPc";
+          window.process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "chatapp-83c84.firebaseapp.com";
+          window.process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID = "chatapp-83c84";
+          window.process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL = "https://chatapp-83c84-default-rtdb.firebaseio.com";
+          `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
